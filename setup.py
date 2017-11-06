bb86b2d4bb6cdcb12c6904279003fdd347f1a548
Pedro Buron <pedro@cornershopapp.com>
2017-11-06 17:37:17 -0300

    move __version__ to __about__

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 # Load the package's __version__.py module as a dictionary.
 about = {}
-with open(os.path.join(here, NAME, '__version__.py')) as f:
+with open(os.path.join(here, NAME, '__about__.py')) as f:
     exec(f.read(), about)
 
 
