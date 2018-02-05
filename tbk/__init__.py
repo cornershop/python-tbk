80167bf055eb15387c230468351aa156eb76876c
Pedro Buron <pedro@cornershopapp.com>
2018-02-05 15:17:16 -0300

    deferred capture service

--- tbk/__init__.py
+++ tbk/__init__.py
@@ -5,6 +5,7 @@ from .commerce import Commerce  # noqa
 from .oneclick import OneClick  # noqa
 from .normal import WebpayNormal  # noqa
 from .nullify import Nullify  # noqa
+from .capture import DeferredCapture  # noqa
 
 INTEGRACION = 'INTEGRACION'
 CERTIFICACION = 'CERTIFICACION'
