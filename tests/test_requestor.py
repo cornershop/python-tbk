bf104de72c81c706294d3ea477daab6f2ffdb491
Pedro Burón <pedro@cornershopapp.com>
2019-08-10 16:59:39 -0400

    replace SoapRequestorTest with function pytest methods

--- tests/test_requestor.py
+++ tests/test_requestor.py
@@ -1,117 +1,162 @@
 import unittest
 
-from tbk.commerce import Commerce
+import pytest
+
 from tbk.soap import SoapRequestor, SoapRequest, SoapResponse, create_soap_requestor
-from tbk.soap.exceptions import SoapClientException, SoapServerException
+from tbk.soap.exceptions import (
+    SoapClientException,
+    SoapServerException,
+    SoapRequestException,
+)
 from tbk.soap.soap_client import SoapClient
 
 from .utils import mock
 
 
-class SoapRequestorTest(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super(SoapRequestorTest, self).__init__(*args, **kwargs)
-        self.addTypeEqualityFunc(SoapResponse, "assert_equal_response")
-        self.addTypeEqualityFunc(SoapRequest, "assert_equal_request")
+@pytest.fixture
+def soap_client():
+    return mock.MagicMock(spec=SoapClient)
 
-    def setUp(self):
-        self.soap_client = mock.MagicMock(spec=SoapClient)
 
-    def test_get_enum_value(self):
-        get_enum_value = self.soap_client.get_enum_value
+def assert_responses(first, second, msg=None):
+    assert first.result == second.result, msg
+    assert first.envelope_sent == second.envelope_sent, msg
+    assert first.envelope_received == second.envelope_received, msg
+    assert first.request.method_name == second.request.method_name, msg
+    assert first.request.args == second.request.args, msg
+    assert first.request.kwargs == second.request.kwargs, msg
 
-        requestor = SoapRequestor(self.soap_client)
-        result = requestor.get_enum_value("EnumType", "value")
 
-        get_enum_value.assert_called_once_with("EnumType", "value")
-        self.assertEqual(get_enum_value.return_value, result)
+@pytest.mark.parametrize(
+    ("enum_type", "value"),
+    (("EnumType", "value"), ("AnIntValue", 1), ("BooleanValue", True)),
+)
+def test_get_enum_value(soap_client, enum_type, value):
+    requestor = SoapRequestor(soap_client)
 
-    def test_get_enum_value_exception(self):
-        get_enum_value = self.soap_client.get_enum_value
-        get_enum_value.side_effect = SoapClientException
+    result = requestor.get_enum_value(enum_type, value)
 
-        requestor = SoapRequestor(self.soap_client)
+    assert soap_client.get_enum_value.return_value == result
 
-        self.assertRaises(
-            SoapClientException, requestor.get_enum_value, "EnumType", "value"
-        )
 
-    def test_create_object(self):
-        create_object = self.soap_client.create_object
+@pytest.mark.parametrize(
+    ("enum_type", "value"),
+    (("EnumType", "value"), ("AnIntValue", 1), ("BooleanValue", True)),
+)
+def test_get_enum_value_exception(soap_client, enum_type, value):
+    soap_client.get_enum_value.side_effect = SoapClientException
+    requestor = SoapRequestor(soap_client)
 
-        requestor = SoapRequestor(self.soap_client)
-        result = requestor.create_object("TypeName", "arg", kw="kw")
+    with pytest.raises(SoapClientException):
+        requestor.get_enum_value(enum_type, value)
 
-        create_object.assert_called_once_with("TypeName", "arg", kw="kw")
-        self.assertEqual(create_object.return_value, result)
 
-    def test_create_object_exception(self):
-        create_object = self.soap_client.create_object
-        create_object.side_effect = SoapClientException
+@pytest.mark.parametrize(
+    ("type_name", "args", "kwargs"),
+    (
+        ("TypeName", ("arg", True, 1), {}),
+        ("AnotherType", (), {"a": "arg", "b": True, "c": 1}),
+        ("YetAnotherType", ("arg1", True, 1), {"a": "arg", "b": True, "c": 1}),
+    ),
+)
+def test_create_object(soap_client, type_name, args, kwargs):
+    requestor = SoapRequestor(soap_client)
+    result = requestor.create_object(type_name, *args, **kwargs)
 
-        requestor = SoapRequestor(self.soap_client)
+    soap_client.create_object.assert_called_once_with(type_name, *args, **kwargs)
+    assert soap_client.create_object.return_value == result
 
-        self.assertRaises(
-            SoapClientException, requestor.create_object, "TypeName", "arg", kw="kw"
-        )
 
-    def test_request(self):
-        request = self.soap_client.request
-        mocked_result = mock.Mock()
-        mocked_envelope_sent = mock.Mock()
-        mocked_envelope_received = mock.Mock()
-        request.return_value = (
-            mocked_result,
-            mocked_envelope_sent,
-            mocked_envelope_received,
-        )
-        args = ("arg",)
-        kwargs = {"kw": "kw"}
-        expected_request = SoapRequest(
-            method_name="methodName", args=args, kwargs=kwargs
-        )
-        expected_response = SoapResponse(
-            request=expected_request,
-            result=mocked_result,
-            envelope_sent=mocked_envelope_sent,
-            envelope_received=mocked_envelope_received,
-        )
+@pytest.mark.parametrize(
+    ("type_name", "args", "kwargs"),
+    (
+        ("TypeName", ("arg", True, 1), {}),
+        ("AnotherType", (), {"a": "arg", "b": True, "c": 1}),
+        ("YetAnotherType", ("arg1", True, 1), {"a": "arg", "b": True, "c": 1}),
+    ),
+)
+def test_create_object_exception(soap_client, type_name, args, kwargs):
+    soap_client.create_object.side_effect = SoapClientException
+    requestor = SoapRequestor(soap_client)
 
-        requestor = SoapRequestor(self.soap_client)
-        response = requestor.request("methodName", *args, **kwargs)
+    with pytest.raises(SoapClientException):
+        requestor.create_object(type_name, *args, **kwargs)
 
-        self.assertEqual(expected_response, response)
 
-    def test_request_server_exception(self):
-        request = self.soap_client.request
-        request.side_effect = SoapServerException(
-            "code", 123, mock.Mock(spec=SoapRequest)
-        )
+@pytest.mark.parametrize(
+    ("method_name", "args", "kwargs"),
+    (
+        ("method1", ("arg", True, 1), {}),
+        ("method2", (), {"a": "arg", "b": True, "c": 1}),
+        ("methodThree", ("arg1", True, 1), {"a": "arg", "b": True, "c": 1}),
+    ),
+)
+def test_request(soap_client, method_name, args, kwargs):
+    mocked_result = mock.Mock()
+    mocked_envelope_sent = mock.Mock()
+    mocked_envelope_received = mock.Mock()
+    soap_client.request.return_value = (
+        mocked_result,
+        mocked_envelope_sent,
+        mocked_envelope_received,
+    )
 
-        requestor = SoapRequestor(self.soap_client)
-        with self.assertRaises(SoapServerException) as ctx:
-            requestor.request("methodName", "arg", kw="kw")
-        self.assertEqual(123, ctx.exception.code)
-        self.assertEqual("code", ctx.exception.error)
+    expected_request = SoapRequest(method_name=method_name, args=args, kwargs=kwargs)
+    expected_response = SoapResponse(
+        request=expected_request,
+        result=mocked_result,
+        envelope_sent=mocked_envelope_sent,
+        envelope_received=mocked_envelope_received,
+    )
+    requestor = SoapRequestor(soap_client)
 
-    def test_request_exception(self):
-        request = self.soap_client.request
-        request.side_effect = Exception
+    response = requestor.request(method_name, *args, **kwargs)
 
-        requestor = SoapRequestor(self.soap_client)
-        with self.assertRaises(Exception):
-            requestor.request("methodName", "arg", kw="kw")
+    assert_responses(expected_response, response)
 
-    def assert_equal_response(self, first, second, msg=None):
-        self.assertEqual(first.result, second.result, msg)
-        self.assertEqual(first.envelope_sent, second.envelope_sent, msg)
-        self.assertEqual(first.envelope_received, second.envelope_received, msg)
-        self.assertEqual(first.request, second.request, msg)
 
-    def assert_equal_request(self, first, second, msg=None):
-        self.assertEqual(first.method_name, second.method_name, msg)
-        self.assertEqual(first.args, second.args, msg)
-        self.assertEqual(first.kwargs, second.kwargs, msg)
+@pytest.mark.parametrize(
+    ("method_name", "args", "kwargs", "error", "code"),
+    (
+        ("method1", ("arg", True, 1), {}, "error1", 1),
+        ("method2", (), {"a": "arg", "b": True, "c": 1}, "error2", 2),
+        ("methodThree", ("arg1", True), {"a": "kwarg", "c": 1}, "error3", 3),
+    ),
+)
+def test_request_server_exception(soap_client, method_name, args, kwargs, error, code):
+    request = mock.Mock(spec=SoapRequest)
+    soap_client.request.side_effect = SoapServerException(error, code, request)
+    requestor = SoapRequestor(soap_client)
+
+    with pytest.raises(SoapServerException) as exc_info:
+        requestor.request(method_name, *args, **kwargs)
+
+    assert exc_info.type is SoapServerException
+    assert exc_info.value.error is error
+    assert exc_info.value.code is code
+    assert exc_info.value.request is request
+
+
+@pytest.mark.parametrize(
+    ("method_name", "args", "kwargs"),
+    (
+        ("method1", ("arg", True, 1), {}),
+        ("method2", (), {"a": "arg", "b": True, "c": 1}),
+        ("methodThree", ("arg1", True, 1), {"a": "arg", "b": True, "c": 1}),
+    ),
+)
+def test_request_exception(soap_client, method_name, args, kwargs):
+    error = Exception()
+    request = mock.Mock(spec=SoapRequest)
+    soap_client.request.side_effect = SoapRequestException(error, request)
+    requestor = SoapRequestor(soap_client)
+
+    with pytest.raises(SoapRequestException) as exc_info:
+        requestor.request(method_name, *args, **kwargs)
+
+    assert exc_info.type is SoapRequestException
+    assert exc_info.value.error is error
+    assert exc_info.value.request is request
 
 
 class SoapResponseTest(unittest.TestCase):
